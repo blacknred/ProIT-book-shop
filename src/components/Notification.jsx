8e987d351192899b28f0e42809f9435b8e41c650
blcknrd <tretyakov.dmi@gmail.com>
2019-05-10 08:35:37 +0300

    implemented notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,3 +1,24 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
-export default () => <div>Notification</div>;
+import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
+
+const Notification = ({ message = 'Warning alert description' }) => (
+    <Alert
+        variant="warning"
+        title="Warning alert title"
+        action={<AlertActionCloseButton onClose={() => ({})} />}
+    >
+        {message}
+    </Alert>
+);
+
+const mapStateToProps = (state, ownProps) => ({
+    // notifications: state.notifications.notificationsList,
+});
+
+const mapDispatchToProps = dispatch => ({
+    // createMessage: (text) => dispatch(createFlashMessage(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notification);
